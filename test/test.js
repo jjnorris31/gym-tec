a0c3a854948720f13a0d203cbbe5a0e0adc81553
rosamariaramirez <rosa.centauri@gmail.com>
2019-11-21 15:01:26 -0600

    tests - jest

---
+++ test/test.js
@@ -0,0 +1,41 @@
+import Login from '../src/views/Login';
+
+function mountComponentWithProps (Component, propsData) {
+    const Constructor = Vue.extend(Component);
+    const vm = new Constructor({
+      propsData
+    }).$mount();
+  
+    return vm.$el;
+  }
+  
+  describe('Login.vue', () => {
+    it('should be the correct login', () => {
+      const headingData = mountComponentWithProps(Login, { color: 'blue' });
+      const styleData = headingData.style.getPropertyValue('color');
+  
+      console.log(styleData)
+  
+      expect(styleData).toEqual('blue');
+    });
+  
+    it('should have the correct title', () => {
+      const headingData = mountComponentWithProps(Login, { title: 'Hola, JuanJo!' });
+      const titleData = headingData.textContent;
+  
+      expect(titleData).toEqual('Hola, JuanJo!');
+    });
+  });
+
+
+  import mutations from './mutations'
+
+test('setItems updates state with items in payload', () => {
+  const state = {
+    items: []
+  }
+  const items = [{}, {}]
+  mutations.setItems(state, { items })
+  expect(state.items).toBe(items)
+})
+
