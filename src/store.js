904540523585cb0232b1b68d7ff8a642c30ffb9d
jjnorris31 <jjnorris31@gmail.com>
2019-11-21 14:46:06 -0600

    add comments to store

--- src/store.js
+++ src/store.js
@@ -19,42 +19,86 @@ export default new Vuex.Store({
   mutations: {
     /**
      * Establece el usuario actual de la aplicación
-     * @param state estado default
+     * @param state ...
      * @param user usuario actual
      */
     setUser(state, user) {
       state.user = user;
     },
+    /**
+     * Establece el plan completo según la rutina
+     * @param state ...
+     * @param fullPlan plan actual
+     */
     setFullPlan(state, fullPlan) {
       console.log(fullPlan);
       state.fullPlan = fullPlan;
     },
+    /**
+     * Establece la rutina seleccionada
+     * @param state
+     * @param routine rutina actual
+     */
     setRoutine(state, routine){
       state.routine = routine;
     },
+    /**
+     * Establece la categoría según la rutina
+     * @param state
+     * @param categories categoria actual
+     */
     setCategories(state, categories) {
       state.categories = categories;
     },
     setCategory(state, category){
       state.category = category;
     },
+    /**
+     * Establecen los ejercicios según la rutina seleccionada
+     * por el usuario
+     * @param state
+     * @param exercises ejercicio actual
+     */
     setExercises(state, exercises){
       state.exercises = exercises;
     },
+    /**
+     * Establece el plan parcial según la rutina
+     * @param state
+     * @param planIndex plan actual
+     */
     setPlanIndex(state, planIndex) {
       state.planIndex = planIndex;
     },
+    /**
+     * Obtiene el índice de la rutina
+     * @param state
+     * @param routineIndex índice de rutina actual
+     */
     setRoutineIndex(state, routineIndex) {
       state.routineIndex = routineIndex;
     }
   },
   actions: {
+    /**
+     * Verifica si las credenciales están hechas correctamente
+     * y de ser así, manda directamente al dashboard
+     * @param context
+     * @param payload
+     */
     signIn(context, payload) {
       firebase.auth.signInWithEmailAndPassword(payload.user, payload.password).then((user) => {
         context.commit('setUser', user);
         router.push('/apphome/dashboard');
       });
     },
+    /**
+     * Obtiene la rutina seleccionada por el usuario desde la base de datos,
+     * además envía directamente a la pantalla de rutinas
+     * @param context
+     * @param payload
+     * @returns {Promise<void>}
+     */
     async selectRoutine(context, payload) {
       context.commit('setRoutine', payload.routineSelected);
       const routineRef = await firebase.firestore.collection('users').doc(firebase.auth.currentUser.uid).collection('routines').doc(payload.routineSelected);
@@ -62,6 +106,11 @@ export default new Vuex.Store({
       context.commit('setCategories', routineDoc.data());
       await router.push('/apphome/categories');
     },
+    /**
+     * Carga los ejercicios según el plan seleccionado por el usuario
+     * @param context
+     * @param payload
+     */
     loadExercises(context, payload) {
       let tempArray = [];
       for (let i = 0; i < payload.category.length; i++) {
@@ -70,30 +119,55 @@ export default new Vuex.Store({
       context.commit('setExercises', tempArray);
       router.push('/apphome/routine');
     },
+    /**
+     * Carga el plan según la categoría seleccionada por el usuario
+     * @param context
+     * @param payload
+     */
     loadPlan(context, payload) {
       context.commit('setCategory', payload.plan);
     },
+    /**
+     * Carga el plan completo según la categoría seleccionada por el usuario
+     * @param context
+     * @param payload
+     */
     loadFullPlan(context, payload) {
       context.commit('setFullPlan', payload.fullPlan);
     },
+    /**
+     * Carga el índice del plan según la categoría seleccionada por el usuario
+     * @param context
+     * @param payload
+     */
     loadPlanIndex(context, payload) {
       context.commit("setPlanIndex", payload.planIndex);
     },
+    /**
+     * Guarda un nuevo ejercicio en la base de datos
+     * @param context
+     * @param payload
+     */
     saveNewExercise(context, payload) {
-
       let planRef = firebase.firestore.collection('users').doc(firebase.auth.currentUser.uid).collection('routines').doc(this.state.routine);
       let updateObject = this.state.fullPlan;
-
       let somePlan = this.state.planIndex;
       updateObject.push(payload);
-
       let tempObject = {somePlan: updateObject};
       planRef.update(tempObject);
       router.push('/apphome/categories');
     },
+    /**
+     * Indica al router que debe de ir a la pantalla de rutinas
+     */
     goRoutines() {
       router.push('/apphome/routines');
     },
+    /**
+     * Registra un nuevo usuario en la base de datos
+     * @param context
+     * @param user
+     */
     signUp(context, user) {
       firebase.auth.createUserWithEmailAndPassword(user.email, user.password)
         .then((event) => {
@@ -110,9 +184,18 @@ export default new Vuex.Store({
           console.log(error);
         });
     },
+    /**
+     * Si el usuario ya inicio sesión ya no es necesario que
+     * inicie otra vez
+     * @param context
+     */
     firstUserSet(context) {
       context.commit('setUser', firebase.auth.currentUser);
     },
+    /**
+     * Cierra la sesión actual del usuario
+     * @param context
+     */
     signOut(context) {
       firebase.auth.signOut().then(() => {
         context.commit('setUser', null);
